46313bd06b4d624701acfa50b6b16bb863c8d453
Projit Chandra <projitchandra32@gmail.com>
2022-01-20 21:24:28 +0530

    Added logout all button

--- src/public/common.js
+++ src/public/common.js
@@ -18,6 +18,24 @@ $(function(){
         event.preventDefault();
     });
 
+    //Logout all devices- onclick
+    $('#logoutAD').click(function(event){
+        const client = new XMLHttpRequest();
+        client.onload = function (){
+                if (this.status != 204) {
+                    
+                    console.error({
+                        code : this.status,
+                        details : JSON.parse(this.responseText)
+                    });
+                }
+                window.location="/";
+            }
+        client.open("DELETE", "/users/logoutAll",true);
+        client.send();
+        event.preventDefault();
+    });
+
     //Months by year - on start
     const availableDataClient = new XMLHttpRequest();
           availableDataClient.onload= function(){
